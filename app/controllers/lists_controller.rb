9587924a22d75b205548bbfa0866ed506063b6e6
Fabrizio Durand <fabrizio.durand9106@gmail.com>
2022-11-17 15:58:10 +0100

    exercice done. Need some front

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,30 @@
+class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmarks = Bookmark.all
+  end
+
+  def create
+    @list = List.new(lists_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def lists_params
+    params.require(:list).permit(:name)
+  end
+end
