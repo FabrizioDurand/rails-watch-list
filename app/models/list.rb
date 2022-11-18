05550f898cebc1ea2c486548cef83a3e58577b04
Fabrizio Durand <fabrizio.durand9106@gmail.com>
2022-11-18 12:49:30 +0100

    some front done, not finished yet

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,6 +3,7 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
   validates :name, presence: true
+  has_one_attached :photo
 end
 
 # A list must have a unique name.
